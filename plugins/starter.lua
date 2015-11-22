b9eb689c249b6891d0af71212b8c5fcf1cb59d50
ArmanIrbot <tgclibot@gmail.com>
2015-11-23 00:51:03 +0330

    Update starter.lua

--- plugins/starter.lua
+++ plugins/starter.lua
@@ -1,7 +1,7 @@
 do
 
 function run(msg, matches)
-  return "Hi😝\nYou Can Invite Users With Me\nBut Now I just Invite By ID\nCreated'Developed and Managed By↙️@ThisIsArman\nFor Any group help send↙️\n/grouphelp"
+  return "Hi😝\nYou Can Invite Users With Me\nBut Now I just Invite By ID\nBot Can Added To Groups With🔽\n!join grouplink\nCreated'Developed and Managed By↙️@ThisIsArman\nFor Any group help send↙️\n/grouphelp"
 end
 
 return {
